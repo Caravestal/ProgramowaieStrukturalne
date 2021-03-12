2522c00f180515ab192899e2dbb2d343b09b81a8
Caravestal <58850165+Caravestal@users.noreply.github.com>
2021-03-12 19:30:00 +0100

    Create new file

---
+++ 2.2.24.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+
+int foo(int n)
+{
+    if(n==1)// w momencie kiedy n zejdzie do 1 w domyśle dodaje n0 czyli 1+1=2 dlatego return 2
+    {
+        return 2; 
+    }
+    return foo(n-1) + n;
+}
+
+int main()
+{
+    printf("%d", foo(3));
+    return 0;
+}
