27583a83216b5aef7e6e48d64dc2b481acef029b
Caravestal <58850165+Caravestal@users.noreply.github.com>
2021-03-24 16:43:24 +0100

    Create wA.2.c

---
+++ wA.2.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int foo(int n)
+{
+    if(n==1 || n==0)
+    {
+        return 2;
+    }
+    if(n%2==0 && n>0)
+    {
+        return foo(n-1) + n;
+    }
+    if(n%2==1 && n>0)
+    {
+        return 2 * foo(n-1) - n;
+    }
+}
+
+int main()
+{
+    printf("%d", foo(3));
+    return 0;
+}
