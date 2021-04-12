f847acefae2b0c7c5a3bc98f7857fc8c7b83f209
Caravestal <58850165+Caravestal@users.noreply.github.com>
2021-04-12 12:47:16 +0200

    Update 4.2.4.c

--- 4.2.4.c
+++ 4.2.4.c
@@ -4,28 +4,18 @@
 
 float foo(int n,int tab[])
 {
-    float temp=1;
-    for(int i=0;i<n;i++)
-        temp*=tab[i];
-    temp=pow(temp,(1/(float)(n)));
-    return temp;
-}
-
-void wypisz(int n, int tab[])
-{
-    for(int i=0;i<n;i++)
-        printf("%i",tab[i]);
-    printf("\n");
+    float temp = 1;
+    for(int i=0; i<n; i++)
+    {
+        temp = temp * tab[i];
+    }
+    return sqrt(temp);
 }
 
 int main()
 {
-    int n=5,tab[5];
-    for(int i=0;i<5;i++)
-    {
-        tab[i]=i+1;
-    }
-    wypisz(n,tab);
-    printf("%f",foo(n,tab));
+    int n = 6;
+    int tab[6] = {2,4,5,5,7,8};
+    printf("%f\n", foo(n, tab));
     return 0;
 }
